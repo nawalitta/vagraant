cad4a6e9ab29755fdf95d94509b3343c6ef3f443
Guillaume <guillaume.pochet@gmail.com>
2016-11-16 14:10:37 +0100

    Addind entity groupe

---
+++ src/RessourceBundle/Entity/Groupe.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace RessourceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * groupe
+ *
+ * @ORM\Table(name="groupe")
+ * @ORM\Entity(repositoryClass="RessourceBundle\Repository\groupeRepository")
+ */
+class Groupe
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="labelG", type="string", length=255)
+     */
+    private $labelG;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set labelG
+     *
+     * @param string $labelG
+     *
+     * @return groupe
+     */
+    public function setLabelG($labelG)
+    {
+        $this->labelG = $labelG;
+
+        return $this;
+    }
+
+    /**
+     * Get labelG
+     *
+     * @return string
+     */
+    public function getLabelG()
+    {
+        return $this->labelG;
+    }
+}
+
