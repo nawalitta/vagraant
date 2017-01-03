a67a0f093df103820d13c69974efd754aac3e849
fpaquere <paquereau.f@gmail.com>
2017-01-03 10:03:11 +0100

    Push merge on master

--- src/CoreBundle/Controller/AffectationController.php
+++ src/CoreBundle/Controller/AffectationController.php
@@ -3,6 +3,7 @@
 namespace CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class AffectationController extends Controller
 {
