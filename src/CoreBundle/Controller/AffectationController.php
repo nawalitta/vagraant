fec1732bb9827a17e753d8502e7f30676036a358
fpaquere <paquereau.f@gmail.com>
2017-01-11 14:56:13 +0100

    add save in database for affectation activte optionnelle

--- src/CoreBundle/Controller/AffectationController.php
+++ src/CoreBundle/Controller/AffectationController.php
@@ -2,6 +2,7 @@
 
 namespace CoreBundle\Controller;
 
+use RessourceBundle\Entity\Enfant;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -9,32 +10,91 @@ class AffectationController extends Controller
 {
     public function affecterEnfantActiviteOptionAction(Request $request)
     {
-        $array = $this->getDataAffecterEnfantActivite();
-        $listActiviteId = $request->get('idActivites');
-        $enfant = $request->get('enfant');
-        $groupe = $request->get('groupe');
-        if($listActiviteId != null){
-            if($enfant != null){
+        (new Enfant());
 
-            }
-            $entityManager = $this->getDoctrine()->getManager();
-            /*foreach ($enfants as $enfant){
-                foreach ($listActiviteId as $id) {
-
-                }
-            }*/
-        }
-        return $this->render('CoreBundle:Affectation:affecter_enfant_activite_option.html.twig', $array );
-    }
-
-    public function affecterEnfantActiviteObligatoireAction()
-    {
-        $array = $this->getDataAffecterEnfantActivite();
-        return $this->render('CoreBundle:Affectation:affecter_enfant_activite_obligatoire.html.twig',$array);
-    }
-
-    private function getDataAffecterEnfantActivite(){
         $entityManager = $this->getDoctrine()->getManager();
+        $array = $this->getDataAffecterEnfantActivite($entityManager);
+        $listActiviteId = $request->get('idActivites');
+        $enfantId = $request->get('enfant');
+        $groupeId = $request->get('groupe');
+        if ($listActiviteId != null) {
+            // Si un enfant particulier a été sélectionné
+            if ($enfantId != "aucun") {
+                // Alors on recherche cet enfant (object) à partir de son id
+                $enfantRepository = $entityManager->getRepository('RessourceBundle:Enfant');
+                $enfant = $enfantRepository->findOneById($enfantId);
+                $activiteRepository = $entityManager->getRepository('ActiviteBundle:Activite');
+                // On ajoute à cet enfant les activitées cochées
+                foreach ($listActiviteId as $activiteId) {
+                    $activite = $activiteRepository->findOneById($activiteId);
+                    $enfant->addActivitesOptionelle($activite);
+                }
+            } else {
+                // Si un groupe particulier a été sélectionné
+                if ($groupeId != "aucun") {
+                    // On reprend la liste des enfants à partir du tableau "$array"
+                    // pour plus d'info voir fonction getDataAffecterEnfantActivite())
+                    $enfants = $array['enfants'];
+                    $activiteRepository = $entityManager->getRepository('ActiviteBundle:Activite');
+                    foreach ($listActiviteId as $activiteId) {
+                        $activite = $activiteRepository->findOneById($activiteId);
+                        foreach ($enfants as $enfant) {
+                            if ($enfant->getGroupe()->getId() == $groupeId) {
+                                $enfant->addActivitesOptionelle($activite);
+                            }
+                        }
+                    }
+                }
+            }
+            $entityManager->flush();
+
+        }
+        return $this->render('CoreBundle:Affectation:affecter_enfant_activite_option.html.twig', $array);
+    }
+
+    public function affecterEnfantActiviteObligatoireAction(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $array = $this->getDataAffecterEnfantActivite($entityManager);
+        $listActiviteId = $request->get('idActivites');
+        $enfantId = $request->get('enfant');
+        $groupeId = $request->get('groupe');
+        if ($listActiviteId != null) {
+            // Si un enfant particulier a été sélectionné
+            if ($enfantId != "aucun") {
+                // Alors on recherche cet enfant (object) à partir de son id
+                $enfantRepository = $entityManager->getRepository('RessourceBundle:Enfant');
+                $enfant = $enfantRepository->findOneById($enfantId);
+                $activiteRepository = $entityManager->getRepository('ActiviteBundle:Activite');
+                // On ajoute à cet enfant les activitées cochées
+                foreach ($listActiviteId as $activiteId) {
+                    $activite = $activiteRepository->findOneById($activiteId);
+                    $enfant->addActivitesObligatoire($activite);
+                }
+            } else {
+                // Si un groupe particulier a été sélectionné
+                if ($groupeId != "aucun") {
+                    // On reprend la liste des enfants à partir du tableau "$array"
+                    // pour plus d'info voir fonction getDataAffecterEnfantActivite())
+                    $enfants = $array['enfants'];
+                    $activiteRepository = $entityManager->getRepository('ActiviteBundle:Activite');
+                    foreach ($listActiviteId as $activiteId) {
+                        $activite = $activiteRepository->findOneById($activiteId);
+                        foreach ($enfants as $enfant) {
+                            if ($enfant->getGroupe()->getId() == $groupeId) {
+                                $enfant->addActivitesObligatoire($activite);
+                            }
+                        }
+                    }
+                }
+            }
+            $entityManager->flush();
+
+        }        return $this->render('CoreBundle:Affectation:affecter_enfant_activite_obligatoire.html.twig', $array);
+    }
+
+    private function getDataAffecterEnfantActivite($entityManager)
+    {
         // Liste des activités
         $activiteRepository = $entityManager->getRepository('ActiviteBundle:Activite');
         $activites = $activiteRepository->findAll();
--- web/js/groupFilter.js
+++ web/js/groupFilter.js
@@ -1,13 +1,16 @@
 function selectGroup(groupId) {
     console.log(groupId);
     var enfants = document.getElementsByName('enfant')[0];
-    for (var index = 0; index < enfants.childElementCount; index++) {
-        var enfantId = enfants[index];
+    for (var index = 1; index < enfants.childElementCount; index++) {
+        var enfant = enfants[index];
         if(groupId == "aucun"){
-            enfants[index].hidden = false;
+            enfant.hidden = false;
         }else {
-            enfants[index].hidden = enfantId.value != groupId;
+            if(enfant.id == groupId) {
+                    enfant.hidden = false;
+            }else{
+                enfant.hidden = true;
+            }
         }
     }
-    enfants[0].hidden = false;
 }
