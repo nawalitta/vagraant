c521e7a65f81d16c4611ff1cc92ddf081e788000
fpaquere <paquereau.f@gmail.com>
2017-01-14 22:14:25 +0100

    modification gui et js

--- src/ActiviteBundle/Controller/ActiviteFixeeController.php
+++ src/ActiviteBundle/Controller/ActiviteFixeeController.php
@@ -2,6 +2,7 @@
 
 namespace ActiviteBundle\Controller;
 
+use MongoDB\Driver\Exception\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use ActiviteBundle\Form\ActiviteFixeeType;
@@ -20,52 +21,42 @@ class ActiviteFixeeController extends Controller
         $enfantRepository = $entityManager->getRepository("RessourceBundle:Enfant");
         $enfants = $enfantRepository->findAll();
         $erreurMsg = "";
-        /**
-        //Recupere le nom de l'enfant pour changer la liste
-        $enfantSelectionner = $enfantRepository->findOneById( $request->get('enfantId'));
-        if($enfantSelectionner !=null)
-        {
-            
-            $activitesFixees = $activiteFixeeRepository->findByEnfant($enfantSelectionner);
-        }
-        */
-        //Récupere la liste des activités coché afin de les supprimer
-        $listActiviteF=$request->get('idActivitesFixee');
-        if($listActiviteF !=null){
-            foreach ($listActiviteF as $id){
 
-                $activiteF = $activiteFixeeRepository->findOneById($id);
-                if ($activiteF != null) {
-                    $entityManager->remove($activiteF);
+        //Récupere la liste des activités coché afin de les supprimer
+        $listActiviteFixeesId = $request->get('idActivitesFixee');
+        if ($listActiviteFixeesId != null) {
+            foreach ($listActiviteFixeesId as $id) {
+                $activiteFixees = $activiteFixeeRepository->findOneById($id);
+                if ($activiteFixees != null) {
+                    $entityManager->remove($activiteFixees);
                 }
-                try{
-                    $entityManager->flush();                   
+                try {
+                    $entityManager->flush();
                 } catch (Exception $ex) {
                     $erreurMsg = "Un enfant possede encore cette activite";
                 }
 
-            }           
+            }
         }
-        
         //Fin suppression des éléments
-        
+
         return $this->render('ActiviteBundle:ActiviteFixee:index.html.twig', array(
-        "activitesFixees"=>$activitesFixees, "enfants" => $enfants,"erreur"=>$erreurMsg
+            "activitesFixees" => $activitesFixees, "enfants" => $enfants, "erreur" => $erreurMsg
         ));
     }
-    
+
     /**
      * Affichage d'une ActiviteFixee présent dans la bdd
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function showAction($id)
+    public
+    function showAction($id)
     {
         $entityManager = $this->getDoctrine()->getManager();
         $activiteFixeeRepository = $entityManager->getRepository("ActiviteBundle:ActiviteFixee");
         $activiteFixee = $activiteFixeeRepository->findOneById($id);
-        $ressourceRepository = $entityManager->getRepository("RessourceBundle:Ressource");
         return $this->render('ActiviteBundle:ActiviteFixee:show.html.twig', array(
-        "activiteFixee"=>$activiteFixee
+            "activiteFixee" => $activiteFixee
         ));
     }
 
@@ -75,37 +66,38 @@ class ActiviteFixeeController extends Controller
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function editAction($idFixee=null,$idActivite,Request $request)
+    public
+    function editAction($idFixee = null, $idActivite, Request $request)
     {
         $entityManager = $this->getDoctrine()->getManager();
         $activiteFixeeRepository = $entityManager->getRepository("ActiviteBundle:ActiviteFixee");
         $activiteFixee = $activiteFixeeRepository->findOneById($idFixee);
-        
-        if($activiteFixee==null){
-            $activiteFixee=new \ActiviteBundle\Entity\ActiviteFixee();
+
+        if ($activiteFixee == null) {
+            $activiteFixee = new \ActiviteBundle\Entity\ActiviteFixee();
         }
-        
-        $form = $this->createForm(ActiviteFixeeType::class,$activiteFixee);
+
+        $form = $this->createForm(ActiviteFixeeType::class, $activiteFixee);
         $form->handleRequest($request);
-        
-        if($form->isValid()){
+
+        if ($form->isValid()) {
             //On ajoute les informations qu'on a présent sur la page
-            $activiteFixee.setHeureDebut($form->get('heureDebut')->getData());
-            $activiteFixee.setHeureFin($form->get('heureDebut')->getData());
-            $activiteFixee.setActivite($idActivite);
-            $activiteFixee.setJour($form->get('jour')->getData());
-            
+            $activiteFixee->setHeureDebut($form->get('heureDebut')->getData());
+            $activiteFixee->setHeureFin($form->get('heureDebut')->getData());
+            $activiteFixee->setActivite($idActivite);
+            $activiteFixee->setJour($form->get('jour')->getData());
+
             $entityManager->persist($activiteFixee);
             $entityManager->flush();
-            
+
             $this->get('session')->getFlashBag()->add('notice', 'ActiviteFixee bien enregistrée.');
 
             return $this->redirect($this->generateUrl('ActiviteBundle_ActiviteFixee_index'));
         }
-        
+
         return $this->render('ActiviteBundle:ActiviteFixee:edit.html.twig', array(
-        'activiteFixee'=>$activiteFixee,
-        'form' => $form->createView()
+            'activiteFixee' => $activiteFixee,
+            'form' => $form->createView()
         ));
     }
 
@@ -113,18 +105,19 @@ class ActiviteFixeeController extends Controller
      * Suppression d'une ActiviteFixee
      * @param null $id
      * @return \Symfony\Component\HttpFoundation\Response
-     */    
-    public function deleteAction($id)
+     */
+    public
+    function deleteAction($id)
     {
         $entityManager = $this->getDoctrine()->getManager();
-  
+
         $activiteFixeeRepository = $entityManager->getRepository("ActiviteBundle:ActiviteFixee");
         $activiteFixee = $activiteFixeeRepository->findOneById($id);
-        if($activiteFixee!=null){
-            $entityManager->remove ($activiteFixee);
+        if ($activiteFixee != null) {
+            $entityManager->remove($activiteFixee);
         }
         $entityManager->flush();
-       
+
         return $this->redirect($this->generateUrl('ActiviteBundle_ActiviteFixee_index'));
     }
 
