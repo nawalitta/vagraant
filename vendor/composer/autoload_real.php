12bce81ab176627bbda9ca77feb287beb91a6c3b
fpaquere <paquereau.f@gmail.com>
2017-01-15 13:59:23 +0100

    Vendor ObHighCharts bundle Add the indew view Add differents charts complete the config files / controllers, etc.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInita48c2d036e6a9d9e9b8b76ba2a69fdc8
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInita48c2d036e6a9d9e9b8b76ba2a69fdc8', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
